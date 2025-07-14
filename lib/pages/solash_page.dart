4406aa739690ade58abdb3cc650b911f94b70686
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-07-14 19:02:51 +0300

    add logo in splah page

--- lib/pages/solash_page.dart
+++ lib/pages/solash_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:login_and_signup/const.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white , 
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 200,) ,
+          Align(
+            alignment: Alignment.topCenter,
+            child: SvgPicture.asset(KLogo ) , 
+            ) , 
+        ],
+      ),
+    ) ;
   }
 }
