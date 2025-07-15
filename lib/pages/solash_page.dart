239f8cedfa1784f1cd14f6a5794fb025fe4993e2
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-07-15 18:33:01 +0300

    add image and text

--- lib/pages/solash_page.dart
+++ lib/pages/solash_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:login_and_signup/widgets/Logo_text.dart';
-import 'package:login_and_signup/widgets/app_color.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -8,19 +8,38 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white , 
-     body: Center(
-       child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(height: 115) ,
-          LogoText() , 
-          
-        
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 115),
+            LogoText(),
+            
+            SvgPicture.asset("assets/image/undraw_access-account_aydp.svg" , height: 500,) , 
 
-        ],
-       ),
-     ),
+            Text("Lorem Ipsum " , style: TextStyle(
+              color: Colors.black , 
+              fontSize: 30 , 
+              fontWeight: FontWeight.bold , 
+              fontFamily: 'Prim' , 
+            ),) , 
+
+            Text("Lorem Ipsum is a dummy text " , style: TextStyle(
+              color: Colors.black , 
+              fontSize: 16 , 
+              
+            ),) ,  
+
+             
+
+            Text("used as placeholder" , style: TextStyle(
+              color: Colors.black , 
+              fontSize: 16 , 
+            ),)
+          ],
+        ),
+      ),
     );
   }
 }
